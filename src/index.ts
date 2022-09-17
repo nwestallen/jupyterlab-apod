a0936c202e66cafa6c94771fff766a8163c3da07
Nick <nwestallen@gmail.com>
2022-09-17 16:02:11 -0400

    Show a picture in the panel

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,15 @@ import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
 
 import { Widget } from '@lumino/widgets';
 
+interface APODResponse {
+  copyright: string;
+  date: string;
+  explanation: string;
+  media_type: 'video' | 'image';
+  title: string;
+  url: string;
+}
+
 /**
  * Initialization data for the jupyterlab_apod extension.
  */
@@ -14,11 +23,36 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab_apod:plugin',
   autoStart: true,
   requires: [ICommandPalette],
-  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
+  activate: async (app: JupyterFrontEnd, palette: ICommandPalette) => {
     console.log('JupyterLab extension jupyterlab_apod is activated!');
 
-    //Create a blank content widget inside of a MainAreaWidget
     const content = new Widget();
+
+    const img = document.createElement('img');
+
+    content.node.appendChild(img);
+
+    const randomDate = () => {
+      const start = new Date(2010, 1, 1);
+      const end = new Date();
+      const randomDate = new Date(
+        start.getTime() + Math.random() * (end.getTime() - start.getTime())
+      );
+      return randomDate.toISOString().slice(0, 10);
+    };
+
+    const response = await fetch(
+      `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${randomDate()}`
+    );
+
+    const data = (await response.json()) as APODResponse;
+
+    if (data.media_type === 'image') {
+      img.src = data.url;
+      img.title = data.title;
+    } else {
+      console.log('Random APOD was not a picture.');
+    }
 
     //Add an application command
     const command = 'apod:open';
